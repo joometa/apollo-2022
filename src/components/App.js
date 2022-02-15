00ba523cf8b5b5fd4a1c930ad615abdcf8197831
joometa <jjlee1623@gmail.com>
2022-02-15 10:14:51 +0900

    file setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom'
+import Detail from '../routes/Detail';
+import Home from '../routes/Home';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/:id" element={<Detail/>} />
+      </Routes>
+    </Router>
   );
 }
 
